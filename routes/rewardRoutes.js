b3dcd9533527775fb894962429e0d2abf673a1f0
Timi16 <timilehinolowu46@gmail.com>
2024-11-12 15:32:27 -0800

    Invite link

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -128,7 +128,7 @@ router.get('/generate-invite/:username',authenticateJWT, async (req, res) => {
         await user.save();
 
         // Construct the invite link directly to index.html
-        const inviteLink = `https://t.me/DolphinsProject_Bot?inviteCode=${inviteCode}`;
+        const inviteLink = `https://dolphins-ai6u.onrender.com?inviteCode=${inviteCode}`;
 
         res.json({ inviteLink });
     } catch (err) {
