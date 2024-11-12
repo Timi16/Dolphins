9dc32603bccd552afb6e7629d3ca893b1d3f3649
Timi16 <timilehinolowu46@gmail.com>
2024-11-12 15:17:02 -0800

    Bot Issues Fixes

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -128,7 +128,7 @@ router.get('/generate-invite/:username',authenticateJWT, async (req, res) => {
         await user.save();
 
         // Construct the invite link directly to index.html
-        const inviteLink = `https://t.me/DolphinsProject_Bot/Dolphins?inviteCode=${inviteCode}`;
+        const inviteLink = `https://t.me/DolphinsProject_Bot?inviteCode=${inviteCode}`;
 
         res.json({ inviteLink });
     } catch (err) {
