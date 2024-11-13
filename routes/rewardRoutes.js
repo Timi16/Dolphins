05bc4a9008eb5c4a1666ff652170d722732e71cc
Timi16 <timilehinolowu46@gmail.com>
2024-11-12 18:28:47 -0800

    Telegram fixation

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -128,7 +128,7 @@ router.get('/generate-invite/:username',authenticateJWT, async (req, res) => {
         await user.save();
 
         // Construct the invite link directly to index.html
-        const inviteLink = `https://dolphins-ai6u.onrender.com?inviteCode=${inviteCode}`;
+        const inviteLink = `https://t.me/DolphinsProject_Bot/Dolphins?inviteCode=${inviteCode}`;
 
         res.json({ inviteLink });
     } catch (err) {
