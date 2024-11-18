d582e1f3cea4c6df7b1ef6e53b6628cf73c72f8c
Timi16 <timilehinolowu46@gmail.com>
2024-11-17 18:27:04 -0800

    Invite Link change

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -199,7 +199,7 @@ router.get('/generate-invite/:username', authenticateJWT, async (req, res) => {
         }
 
         // Always use the same format for invite link
-        const inviteLink = `https://dolphins-ai6u.onrender.com?invite=${user.inviteCode}`;
+        const inviteLink = `https://dolphins-ai6u.onrender.com/?inviteCode=${user.inviteCode}`;
 
         res.json({ 
             inviteLink,
