8afe175748d2b54a593b91bbbc670f88ad7ffe35
Timi16 <timilehinolowu46@gmail.com>
2024-11-18 11:38:10 -0800

    Invite Link change To Telegram

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -199,7 +199,7 @@ router.get('/generate-invite/:username', authenticateJWT, async (req, res) => {
         }
 
         // Always use the same format for invite link
-        const inviteLink = `https://dolphins-ai6u.onrender.com/?inviteCode=${user.inviteCode}`;
+        const inviteLink = `https://t.me/DolphinsProject_Bot/Dolphins/?startapp=${user.inviteCode}`;
 
         res.json({ 
             inviteLink,
